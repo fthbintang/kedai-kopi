b5cb9f9a392cf5e5a009487e651741470c5fcd0a
fthbintang <bintangfathehah@gmail.com>
2023-10-03 21:23:56 +0800

    menampilkan halaman pada tiap sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AtributController;
+use App\Http\Controllers\TembakauController;
+use App\Http\Controllers\BahanBakuController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PembelianKopiController;
+use App\Http\Controllers\PembelianTembakauController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
@@ -22,3 +27,14 @@ Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
+Route::get('/atribut', [AtributController::class, 'index'])->middleware('auth');
+
+Route::get('/bahan-baku', [BahanBakuController::class, 'index'])->middleware('auth');
+
+Route::get('/tembakau', [TembakauController::class, 'index'])->middleware('auth');
+
+Route::get('/pembelian-kopi', [PembelianKopiController::class, 'index'])->middleware('auth');
+
+Route::get('/pembelian-tembakau', [PembelianTembakauController::class, 'index'])->middleware('auth');
+
