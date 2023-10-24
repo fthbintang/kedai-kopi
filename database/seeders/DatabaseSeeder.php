332d4e6b0fbd87d2557d9b947b59e9d527cea59a
Nikko Lweiss <nikko146@outlook.com>
2023-10-24 15:57:14 +0800

    Make New Condition for Level in Profile Page, and Add New Data in Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,14 +19,28 @@ class DatabaseSeeder extends Seeder
             'name' => 'Muhammad Bintang Fathehah',
             'username' => 'bintang',
             'password' => bcrypt('bintang'),
-            'level' => 'admin'
+            'level' => '1'
         ]);
 
         User::create([
             'name' => 'Ahmad Ariyanur Rahman',
             'username' => 'ari',
             'password' => bcrypt('ari'),
-            'level' => 'admin'
+            'level' => '1'
+        ]);
+
+        User::create([
+            'name' => 'Owner',
+            'username' => 'owner',
+            'password' => bcrypt('owner'),
+            'level' => '2'
+        ]);
+
+        User::create([
+            'name' => 'Pekerja',
+            'username' => 'pekerja',
+            'password' => bcrypt('pekerja'),
+            'level' => '3'
         ]);
 
         Barang::create([
@@ -41,6 +55,20 @@ class DatabaseSeeder extends Seeder
             'stok' => 10,
             'unit' => 'Kg',
             'jenis' => 'Atribut'
+        ]);
+
+        Barang::create([
+            'nama_barang' => 'Biji Kopi Aceh Gayo',
+            'stok' => 5,
+            'unit' => 'Kg',
+            'jenis' => 'Bahan Baku'
+        ]);
+
+        Barang::create([
+            'nama_barang' => 'LA Ice Purpleboost',
+            'stok' => 20,
+            'unit' => 'Kg',
+            'jenis' => 'Tembakau'
         ]);
     }
 }
